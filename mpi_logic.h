a59b91e98725f22137e6bb24f6549069d71d9f38
Giuseppe Napoli <62253855+Peppen@users.noreply.github.com>
2022-01-23 16:03:55 +0100

    Delete some functions

---
+++ mpi_logic.h
@@ -0,0 +1,17 @@
+#include "mpi_chunk.h"
+
+typedef struct Word {
+  char word[LINE_LIMIT];
+  unsigned long int occurrences;
+} word;
+
+typedef struct Occurrence {
+  word word;
+  struct Occurrence *next;
+} occurrence;
+
+void count_words(occurrence **maps, chunk *myChunks, int myChunkNumber, char *dirPath);
+int getWordsNumber(occurrence *words);
+void createWordDatatype(MPI_Datatype *wordData);
+void toLower(char *s);
+void del(char *line);
